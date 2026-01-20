39bfc3bf5cc32d1c63595b7095118b8ff163419a
Oussama El Banna <oel-bann@c4r10p3.1337.ma>
2026-01-20 19:55:42 +0100

    tokenizing

--- Tokenizing/Tokenizing.cpp
+++ Tokenizing/Tokenizing.cpp
@@ -1,4 +1,30 @@
 #include "Tokenizing.hpp"
 
-Tokenizing::Tokenizing(std::string filename): filename(filename)
-{}
+Tokenizing::Tokenizing(std::string filepath): _filepath(filepath)
+{
+    openConfFile();
+}
+
+void Tokenizing::openConfFile()
+{
+    _file.open(_filepath);
+    if (!_file.is_open())
+    {
+        Error::printError("The ConfigFile Can't Open");
+        exit(1);
+    }
+}
+
+const std::vector<std::string>& Tokenizing::get_tokens() const
+{
+    return (_tokens);
+}
+
+void Tokenizing::split_tokens()
+{
+    while (std::getline(std::cin))
+    {
+        /* code */
+    }
+    
+}
