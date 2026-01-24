edf4e2273aabf0d827778ff30458816d84c63b29
ilyas <meslakiami@gmail.com>
2026-01-25 00:55:18 +0100

    add autoindex and return keywords to validation

--- Validation/Validation.cpp
+++ Validation/Validation.cpp
@@ -3,19 +3,30 @@
 Validation::Validation(std::vector<std::string> inputData)
     :  _idx(0), _level(0), _data(inputData) 
 {
-    createUsedMap();
+    CreateMap();
+    createServerdMap();
+    createLocationMap();
 }
 
-void    Validation::createUsedMap()
+void    Validation::createLocationMap()
 {
-    _used["Server"] = false;
-    _used["Listen"] = false;
-    _used["Server_name"] = false;
-    _used["RootServer"] = false;
-    _used["IndexServer"] = false;
-    _used["Location"] = false;
-    _used["RootLocation"] = false;
-    _used["IndexLocation"] = false;
+    _useLocation["Location"] = false;
+    _useLocation["Root"] = false;
+    _useLocation["Index"] = false;
+    _useLocation["Autoindex"] = false;
+    _useLocation["Return"] = false;
+
+}
+
+void    Validation::createServerdMap()
+{
+    _useServer["Server"] = false;
+    _useServer["Listen"] = false;
+    _useServer["Server_name"] = false;
+    _useServer["Root"] = false;
+    _useServer["Index"] = false;
+    _useServer["Autoindex"] = false;
+    _useServer["Return"] = false;
 }
 
 void    Validation::CreateMap()
@@ -26,31 +37,52 @@ void    Validation::CreateMap()
     _map["root"] = &Validation::IsValidRoot;
     _map["index"] = &Validation::IsValidIndex;
     _map["location"] = &Validation::IsValidLocation;
+    _map["autoindex"] = &Validation::IsValidAutoindex;
+    _map["return"] = &Validation::IsValidReturn;
+    // _map["error_pages"] = &Validation::;
+    // _map["client_max_body_size"] = &Validation::;
     
+}
+
+bool    SkipedOptions(std::string option)
+{
+    if ( option == "Return" || option == "Autoindex" )
+        return ( true );
+    return ( false );
 }
 
 void Validation::CheckExistance(std::pair<std::string, bool> used)
 {
-    if (used.first !=  "IndexLocation" && used.first !=  "RootLocation" && used.second == false)
-       Error::ThrowError("Invalid Syntax : (Missing Element in Server)");
+    if (!SkipedOptions( used.first ) && used.second == false )
+        Error::ThrowError("Invalid Syntax : (Missing Element in Server)");
+}
+
+void     Validation::CheckLevelAndDuplication(bool& first, bool& second, std::string msg)
+{
+    if ( _level == 0) {
+        Error::ThrowError("Invalid Syntax : (Element out of scope)");
+    };
+	CkeckDuplication(first, second, "Invalid Syntax : ( Duplication In " + msg +" )");
+    _idx++;
 }
 
 void    Validation::ResetServerSeting()
 {
-    std::for_each(_used.begin(), _used.end(), CheckExistance);
+    std::for_each(_useServer.begin(), _useServer.end(), CheckExistance);
 
-    _used["RootServer"] = false;
-    _used["IndexServer"] = false;
+    _useServer["Root"] = false;
+    _useServer["Index"] = false;
 }
 void    Validation::ResetLocationSeting()
 {
-    _used["RootLocation"] = false;
-    _used["IndexLocation"] = false;
+    _useLocation["Root"] = false;
+    _useLocation["Index"] = false;
 }
 
+//      SERVER
 void    Validation::IsValidServer()
 {
-    _used["Server"] = true;
+    _useServer["Server"] = true;
     _idx++;
     if ( _level != 0 || _data[_idx] != "{" )
        Error::ThrowError("Invalid Syntax");
@@ -58,43 +90,43 @@ void    Validation::IsValidServer()
         _level++;
     _idx++;
     
-    ScopValidation();
+    CheckValidation();
     ResetServerSeting();
-
 }
 
+//      LOCATION
 void    Validation::IsValidLocation()
 {
-    _used["Location"] = true;
+    _useLocation["Location"] = true;
     _idx++;
     if ( _level != 1 || IsSeparator() )
-       Error::ThrowError("Invalid Syntax");
+       Error::ThrowError("Invalid Syntax : (Invalid Location)");
     _idx++;
     if ( _data[_idx] != "{" )
-       Error::ThrowError("Invalid Syntax");
+       Error::ThrowError("Invalid Syntax : (missing '{' In location )");
     else
         _level++;
     _idx++;
     
-    ScopValidation();
+    CheckValidation();
     ResetLocationSeting();
 }
 
-long long   Validation::ConvertToNumber(std::string num)
+long   Validation::ConvertToNumber(std::string num)
 {
     char* endptr;
     errno = 0;
 
-    long long port = std::strtoll(num.c_str(), &endptr, 10);
+    long port = std::strtoll(num.c_str(), &endptr, 10);
 
-    if ( errno != 0 || *endptr != '\0' || num[0] == '+' )
+    if ( num[0] == '+' || errno != 0 || *endptr != '\0' )
         Error::ThrowError("Invalid Syntax ( Number Not Valid )");
     return ( port );
 }
 
 void Validation::PortOnly()
 {
-    long long port = ConvertToNumber(_data[_idx]);
+    long port = ConvertToNumber(_data[_idx]);
     
     if ( port < 0 || port > 65535 )
         Error::ThrowError("Invalid Syntax ( Port Number Out Of Range )");
@@ -104,7 +136,7 @@ void Validation::PortOnly()
 
 void    Validation::ValidIP()
 {
-    long long Ip = 0;
+    long Ip = 0;
     size_t countPoint = 0;
     size_t start = 0;
     size_t pos  = 0;
@@ -135,11 +167,12 @@ void Validation::IpAndPort()
     PortOnly();
 }
 
+//     LISTEN
 void    Validation::IsValidListen()
 {
-    _used["Listen"] = true;
+    _useServer["Listen"] = true;
     if ( _level != 1)
-        Error::ThrowError("Invalid Syntax : (Invalid Scoop)");
+        Error::ThrowError("Invalid Syntax : (Element out of scope)");
     _idx++;
 
     if (  _idx + 1 < (int)_data.size() )
@@ -164,11 +197,12 @@ bool Validation::IsSeparator()
     return ( false );
 }
 
+//      SEVERNAME
 void    Validation::IsValidServerName()
 {
-    _used["Server_name"] = true;
+    _useServer["Server_name"] = true;
     if ( _level != 1)
-        Error::ThrowError("Invalid Syntax : (Invalid Scoop)");
+        Error::ThrowError("Invalid Syntax : (Element out of scope)");
     _idx++;
 
     if ( _data[_idx] == ";" )
@@ -198,16 +232,13 @@ void    Validation::CkeckDuplication(bool& first, bool& second, std::string msg)
         return ;
     }
     else
-        Error::ThrowError("Invalid Syntax : (Invalid Scoop)");
+        Error::ThrowError("Invalid Syntax : (Element out of scope)");
 }
 
+//      ROOT
 void    Validation::IsValidRoot()
 {
-    if ( _level == 0) {
-        Error::ThrowError("Invalid Syntax : (Invalid Scoop)");
-    };
-	CkeckDuplication( _used["RootServer"], _used["RootLocation"], "Invalid Syntax : (Duplication In Root Path)");
-    _idx++;
+    CheckLevelAndDuplication(_useServer["Root"], _useLocation["Root"], "Root"); 
 
     if ( IsSeparator() )
         Error::ThrowError("Invalid Syntax : (Root Has Invalid Path)");
@@ -216,14 +247,10 @@ void    Validation::IsValidRoot()
         _idx++;
 }
 
+//      INDEX
 void    Validation::IsValidIndex()
 {
-    if ( _level == 0 ) {
-        Error::ThrowError("Invalid Syntax : (Invalid Scoop)");
-    }
-
-	CkeckDuplication(_used["IndexServer"], _used["IndexLocation"], "Invalid Syntax : (Duplication In Index File)");
-    _idx++;
+    CheckLevelAndDuplication(_useServer["Index"], _useLocation["Index"], "Index");
 
     if ( _data[_idx] == ";" )
         Error::ThrowError("Invalid Syntax : (Index Must Have a Name)");
@@ -238,7 +265,42 @@ void    Validation::IsValidIndex()
 
 }
 
-void    Validation::ScopValidation()
+void    Validation::IsValidAutoindex()
+{
+    CheckLevelAndDuplication(_useServer["Autoindex"], _useLocation["Autoindex"], "Autoindex");
+    
+    if ( IsSeparator() || (_data[_idx] != "off" && _data[_idx] != "on") )
+        Error::ThrowError("Invalid Syntax : (autoindex Has Invalid option )");
+    _idx++;
+    if ( _data[_idx] == ";" )
+        _idx++;
+}
+
+// void    Validation::CheckURL()
+// {
+//     if ( _data[_idx].find("http://") == 0 || _data[_idx].find("https://") == 0 || _data[_idx][0] == '/' )
+//         _idx++;
+//     else
+//         Error::ThrowError("Invalid Syntax : ( Invalid Redirection URL In return )");
+// }
+
+void    Validation::IsValidReturn()
+{
+    CheckLevelAndDuplication(_useServer["Return"], _useLocation["Return"], "Return");
+
+    long errorCode = ConvertToNumber( _data[_idx]);
+    if ( errorCode != 301 && errorCode != 302 )
+        Error::ThrowError("Invalid Syntax : ( Invalid Redirection Code In return )");
+    _idx++;
+
+    if ( !IsSeparator() )
+        _idx++;
+    if ( _data[_idx] == ";" )
+        _idx++;
+
+}
+
+void    Validation::CheckValidation()
 {    
     while( _idx < (int)_data.size() )
     {
@@ -261,10 +323,4 @@ void    Validation::ScopValidation()
     if ( _level != 0 )
         Error::ThrowError("Invalid Syntax");
 
-}
-
-void    Validation::CheckValidation()
-{
-    CreateMap();
-    ScopValidation();
 }
--- Validation/Validation.hpp
+++ Validation/Validation.hpp
@@ -6,7 +6,8 @@ class Validation {
     private:
         int     _idx;
         int     _level;
-        std::map<std::string, bool> _used;
+        std::map<std::string, bool> _useServer;
+        std::map<std::string, bool> _useLocation;
         std::vector<std::string> _data;
         
         typedef std::map<std::string, void (Validation::*)() >Map;
@@ -14,27 +15,33 @@ class Validation {
         
         static void     CheckExistance(std::pair<std::string, bool>);
         void            CreateMap();
-        void            createUsedMap();
-        void            ScopValidation();
+        void            createServerdMap();
+        void            createLocationMap();
         
         void            IsValidServer();
         void            ResetServerSeting();
         
         void            IsValidLocation();
         void            ResetLocationSeting();
-
+        
+        void            IsValidReturn();
+        void            CheckURL();
+        
         void            IsValidIndex();
         void            IsValidRoot();
+        void            IsValidAutoindex();
         void            CkeckDuplication(bool& first, bool& second, std::string msg);
+        void            CheckLevelAndDuplication(bool& first, bool& second, std::string msg);
 
         void            IsValidServerName();
         bool            IsSeparator();
+
 
         void            IsValidListen();
         void            IpAndPort();
         void            PortOnly();
         void            ValidIP();
-        long long       ConvertToNumber(std::string num);
+        long            ConvertToNumber(std::string num);
 
     public:
     
