40b62b76a1bc9e8f549cd4d0c8aba0a63ff7cf73
alaeeddine azzaoui <aazzaoui@localhost.localdomain>
2026-01-27 22:41:14 +0100

    update parsing

--- Parsing/Parsing.hpp
+++ Parsing/Parsing.hpp
@@ -12,7 +12,7 @@ public:
 	Parsing();
 	static AST<std::string>& AddServer();
 	static std::vector<std::string> GetDirecAgs(AST<std::string>& node, std::string& direcName);
-	static std::vector<AST<std::string>> GetNode(AST<std::string>& node, std::string& nodeName);
+	static std::vector<AST<std::string> > GetNode(AST<std::string>& node, std::string& nodeName);
 	static AST<std::string>& AddLocation(AST<std::string>& server);
 	static AST<std::string>& AddDirective(AST<std::string>& node, std::string value);
 	static void AddArg(AST<std::string>& node, std::string arg);
