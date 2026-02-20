bc7c71359788a02ecda974806fd41a87db8262c7
alaeeddine azzaoui <aazzaoui@localhost.localdomain>
2026-02-20 06:22:11 +0000

    improve the multiplexer code

--- SessionManagement/SessionManagemer.cpp
+++ SessionManagement/SessionManagemer.cpp
@@ -1,92 +1,113 @@
 #include "SessionManager.hpp"
 
-SessionManager* SessionManager::instance = NULL;
+SessionManager *SessionManager::instance = NULL;
 
 SessionManager::SessionManager() : SESSION_TIMEOUT(3600) {}
 
-SessionManager::~SessionManager() {
-    for (map<string, Session*>::iterator it = sessions.begin(); it != sessions.end(); ++it) {
-        delete it->second;
-    }
-    sessions.clear();
+SessionManager::~SessionManager()
+{
+	for (map<string, Session *>::iterator it = sessions.begin(); it != sessions.end(); ++it)
+	{
+		delete it->second;
+	}
+	sessions.clear();
 }
 
-SessionManager* SessionManager::getInstance() {
-    if (instance == NULL) {
-        instance = new SessionManager();
-    }
-    return instance;
+SessionManager *SessionManager::getInstance()
+{
+	if (instance == NULL)
+	{
+		instance = new SessionManager();
+	}
+	return instance;
 }
 
-string SessionManager::generateSessionId() {
-    stringstream ss;
-    srand(time(NULL) + rand());
-    for (int i = 0; i < 16; i++) {
-        int random = rand() % 256;
-        ss << hex << setw(2) << setfill('0') << random;
-    }
-    return ss.str();
+string SessionManager::generateSessionId()
+{
+	stringstream ss;
+	srand(time(NULL) + rand());
+	for (int i = 0; i < 16; i++)
+	{
+		int random = rand() % 256;
+		ss << hex << setw(2) << setfill('0') << random;
+	}
+	return ss.str();
 }
 
-Session* SessionManager::createSession() {
-    string sessionId = generateSessionId();
-    Session* session = new Session(sessionId);
-    sessions[sessionId] = session;
-    return session;
+Session *SessionManager::createSession()
+{
+	string sessionId = generateSessionId();
+	Session *session = new Session(sessionId);
+	sessions[sessionId] = session;
+	return session;
 }
 
-Session* SessionManager::getSession(const string &sessionId) {
-    map<string, Session*>::iterator it = sessions.find(sessionId);
-    if (it == sessions.end()) {
-        return NULL;
-    }
-    
-    Session* session = it->second;
-    
-    // Check if expired
-    time_t now = time(NULL);
-    if (now - session->lastAccessedAt > SESSION_TIMEOUT) {
-        delete session;
-        sessions.erase(it);
-        return NULL;
-    }
-    
-    // Update last accessed time
-    session->lastAccessedAt = now;
-    return session;
+Session *SessionManager::getSession(const string &sessionId)
+{
+	map<string, Session *>::iterator it = sessions.find(sessionId);
+	if (it == sessions.end())
+	{
+		return NULL;
+	}
+
+	Session *session = it->second;
+
+	// Check if expired
+	time_t now = time(NULL);
+	if (now - session->lastAccessedAt > SESSION_TIMEOUT)
+	{
+		delete session;
+		sessions.erase(it);
+		return NULL;
+	}
+
+	// Update last accessed time
+	session->lastAccessedAt = now;
+	return session;
 }
 
-void SessionManager::destroySession(const string &sessionId) {
-    map<string, Session*>::iterator it = sessions.find(sessionId);
-    if (it != sessions.end()) {
-        delete it->second;
-        sessions.erase(it);
-    }
+void SessionManager::destroySession(const string &sessionId)
+{
+	map<string, Session *>::iterator it = sessions.find(sessionId);
+	if (it != sessions.end())
+	{
+		delete it->second;
+		sessions.erase(it);
+	}
 }
 
-void SessionManager::cleanupExpiredSessions() {
-    time_t now = time(NULL);
-    for (map<string, Session*>::iterator it = sessions.begin(); it != sessions.end(); ) {
-        if (now - it->second->lastAccessedAt > SESSION_TIMEOUT) {
-            delete it->second;
-            it = sessions.erase(it);
-        } else {
-            ++it;
-        }
-    }
+void SessionManager::cleanupExpiredSessions()
+{
+	time_t now = time(NULL);
+	for (map<string, Session *>::iterator it = sessions.begin(); it != sessions.end();)
+	{
+		if (now - it->second->lastAccessedAt > SESSION_TIMEOUT)
+		{
+			delete it->second;
+			sessions.erase(it);
+		}
+		else
+		{
+			++it;
+		}
+	}
 }
 
-void SessionManager::setSessionData(const string &sessionId, const string &key, const string &value) {
-    Session* session = getSession(sessionId);
-    if (session) {
-        session->data[key] = value;
-    }
+void SessionManager::setSessionData(const string &sessionId, const string &key, const string &value)
+{
+	Session *session = getSession(sessionId);
+	if (session)
+	{
+		session->data[key] = value;
+	}
 }
 
-string SessionManager::getSessionData(const string &sessionId, const string &key) {
-    Session* session = getSession(sessionId);
-    if (session && session->data.find(key) != session->data.end()) {
-        return session->data[key];
-    }
-    return "";
+string SessionManager::getSessionData(const string &sessionId, const string &key)
+{
+	Session *session = getSession(sessionId);
+	if (session && session->data.find(key) != session->data.end())
+	{
+		return session->data[key];
+	}
+	return "";
 }
